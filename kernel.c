843a69db65b5ac3734a3f7006a50fe4231cd7a0e
Davide Ficarra <ficarra.davide290@gmail.com>
2025-10-13 22:55:26 +0200

    Added function to invoke sbi calls, added printing to debug console

--- kernel.c
+++ kernel.c
@@ -1,3 +1,5 @@
+#include "kernel.h"
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
@@ -8,6 +10,39 @@ typedef uint32_t size_t;
  *   __stack_top[]: first available address after kernel code and data
  */
 extern char __bss[], __bss_end[], __stack_top[];
+
+/* Sets register value to invoke a sbi call. Registers `a0` and `a1` can be changed when invoking
+ * the call because in those registers will be eventually write an error value or the value returned
+ * by the call. Those values are stored in this kernel with the structure `sbiret`. */
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3,
+    long arg4, long arg5, long fid, long eid)
+{
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+
+    __asm__ __volatile__(
+        "ecall"
+        : "=r"(a0), "=r"(a1)                  // Stores "return" values in `a0` and `a1`
+        : "r"(a0), "r"(a1), "r"(a2), "r"(a3), /* Loads in every register the value passed */
+          "r"(a4), "r"(a5), "r"(a6), "r"(a7)  /* as argument for that register. */
+        : "memory"
+    );
+
+    return (struct sbiret){.error = a0, .value = a1};
+}
+
+// Set values to registers to invoke `Console Putchar` to print to the debug console
+// the passed character `c`
+void putchar(char c)
+{
+    sbi_call(c, 0, 0, 0, 0, 0, 0, 1);
+}
 
 // Writes character `c` in memory addresses `buf`...`buf`+`n`
 void *memset(void *buf, char c, size_t n)
@@ -25,7 +60,13 @@ void kernel_main(void)
     // Writes into .bss section value 0, some bootloaders do it automatically
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    for (;;);
+    // Prints `*s` to the console
+    const char *s = "\nHello world!\n";
+    for (size_t i = 0; s[i] != '\0'; i++)
+        putchar(s[i]);
+
+    for (;;)
+        __asm__ __volatile__("wfi");
 }
 
 // Starting point
