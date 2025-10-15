325ee34943c690be04c33e66305fae9e3df76fa0
Davide Ficarra <ficarra.davide290@gmail.com>
2025-10-15 16:32:50 +0200

    Added exception handling

--- kernel.c
+++ kernel.c
@@ -48,6 +48,100 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3,
     return (struct sbiret){.error = a0, .value = a1};
 }
 
+__attribute__((naked))
+__attribute__((aligned(4)))
+void kernel_entry(void)
+{
+    __asm__ __volatile__(
+        // Saves state of registers into memory, including stack pointer.
+        "csrw sscratch, sp\n"
+        "addi sp, sp, -4 * 31\n"
+        "sw ra,  4 * 0(sp)\n"
+        "sw gp,  4 * 1(sp)\n"
+        "sw tp,  4 * 2(sp)\n"
+        "sw t0,  4 * 3(sp)\n"
+        "sw t1,  4 * 4(sp)\n"
+        "sw t2,  4 * 5(sp)\n"
+        "sw t3,  4 * 6(sp)\n"
+        "sw t4,  4 * 7(sp)\n"
+        "sw t5,  4 * 8(sp)\n"
+        "sw t6,  4 * 9(sp)\n"
+        "sw a0,  4 * 10(sp)\n"
+        "sw a1,  4 * 11(sp)\n"
+        "sw a2,  4 * 12(sp)\n"
+        "sw a3,  4 * 13(sp)\n"
+        "sw a4,  4 * 14(sp)\n"
+        "sw a5,  4 * 15(sp)\n"
+        "sw a6,  4 * 16(sp)\n"
+        "sw a7,  4 * 17(sp)\n"
+        "sw s0,  4 * 18(sp)\n"
+        "sw s1,  4 * 19(sp)\n"
+        "sw s2,  4 * 20(sp)\n"
+        "sw s3,  4 * 21(sp)\n"
+        "sw s4,  4 * 22(sp)\n"
+        "sw s5,  4 * 23(sp)\n"
+        "sw s6,  4 * 24(sp)\n"
+        "sw s7,  4 * 25(sp)\n"
+        "sw s8,  4 * 26(sp)\n"
+        "sw s9,  4 * 27(sp)\n"
+        "sw s10, 4 * 28(sp)\n"
+        "sw s11, 4 * 29(sp)\n"
+
+        "csrr a0, sscratch\n"
+        "sw a0, 4 * 30(sp)\n"
+
+        // Calls `handle_trap` method with parameter `a0`, pointer to the last
+        // memory address of the stack.
+        "mv a0, sp\n"
+        "call handle_trap\n"
+
+        // Restores register values from memory
+        "lw ra,  4 * 0(sp)\n"
+        "lw gp,  4 * 1(sp)\n"
+        "lw tp,  4 * 2(sp)\n"
+        "lw t0,  4 * 3(sp)\n"
+        "lw t1,  4 * 4(sp)\n"
+        "lw t2,  4 * 5(sp)\n"
+        "lw t3,  4 * 6(sp)\n"
+        "lw t4,  4 * 7(sp)\n"
+        "lw t5,  4 * 8(sp)\n"
+        "lw t6,  4 * 9(sp)\n"
+        "lw a0,  4 * 10(sp)\n"
+        "lw a1,  4 * 11(sp)\n"
+        "lw a2,  4 * 12(sp)\n"
+        "lw a3,  4 * 13(sp)\n"
+        "lw a4,  4 * 14(sp)\n"
+        "lw a5,  4 * 15(sp)\n"
+        "lw a6,  4 * 16(sp)\n"
+        "lw a7,  4 * 17(sp)\n"
+        "lw s0,  4 * 18(sp)\n"
+        "lw s1,  4 * 19(sp)\n"
+        "lw s2,  4 * 20(sp)\n"
+        "lw s3,  4 * 21(sp)\n"
+        "lw s4,  4 * 22(sp)\n"
+        "lw s5,  4 * 23(sp)\n"
+        "lw s6,  4 * 24(sp)\n"
+        "lw s7,  4 * 25(sp)\n"
+        "lw s8,  4 * 26(sp)\n"
+        "lw s9,  4 * 27(sp)\n"
+        "lw s10, 4 * 28(sp)\n"
+        "lw s11, 4 * 29(sp)\n"
+        "lw sp,  4 * 30(sp)\n"
+        "sret\n"
+    );
+}
+
+// I could comment the parameter and the passing of it when doing `mv a0, sp` in `kernel_entry`
+// since it is unused now, but it will be useful when implementing syscalls
+void handle_trap(struct trap_frame *f)
+{
+    uint32_t scause = READ_CSR(scause);
+    uint32_t stval = READ_CSR(stval);
+    uint32_t user_pc = READ_CSR(sepc);
+
+    PANIC("unexpected trap scause=0x%x, stval=0x%x, sepc=0x%x\n", scause, stval, user_pc);
+}
+
 // Set values to registers to invoke `Console Putchar` to print to the debug console
 // the passed character `c`
 void putchar(char c)
@@ -60,12 +154,16 @@ void kernel_main(void)
     // Writes into .bss section value 0, some bootloaders do it automatically
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
+    // Sets register `stvec` to point at `kernel_entry`, so when an exception
+    // is generated the next instructions will be those strarting from address
+    // stored in `stvec`
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
     printf("\nHello %s\n", "World!");
     printf("1 + 2 = %d\n", 1 + 2);
     printf("Pointer: %x\n", 0x1234abcd);
 
-    PANIC("Booted!");        // Kernel panics and "stops" here.
-    printf("Not printed\n");
+    __asm__ __volatile__("unimp"); // Generates a "illegal instruction" exception
 
     for (;;)
         __asm__ __volatile__("wfi");
